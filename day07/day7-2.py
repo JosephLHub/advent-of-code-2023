93dec53d97f82d2a37bf0acd4fd0aa388a800b2d
JosephLHub <125474970+JosephLHub@users.noreply.github.com>
2023-12-07 16:29:50 +0000

    renamed files & completed day 7

---
+++ day07/day7-2.py
@@ -0,0 +1,47 @@
+import sys
+hand_info = sys.stdin.read().split("\n")
+info_dict = {}
+cards = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+index = ["m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"]
+
+for info in hand_info:
+    info_dict.update({info[:info.index(" ")] : int(info[info.index(" ") + 1 :])}) #Add card string and bid number to dictionary
+rank_dict = {}
+for hand in info_dict.keys():
+    counts = []
+    for card in cards:
+        counts.append(hand.count(card))
+    if counts[12] > 0:
+        temp = counts[12]
+        counts[12] = 0
+        counts[counts.index(max(counts))] += temp
+    if 5 in counts or (4 in counts and counts[12] >= 1):
+        rank_dict.update({hand : 6})
+    elif 4 in counts:
+        rank_dict.update({hand : 5})
+    elif 3 in counts and 2 in counts:
+        rank_dict.update({hand : 4})
+    elif 3 in counts:
+        rank_dict.update({hand : 3})
+    elif counts.count(2) == 2:
+        rank_dict.update({hand : 2})
+    elif counts.count(2) == 1:
+        rank_dict.update({hand : 1})
+    else:
+        rank_dict.update({hand : 0})
+rank_dict = dict(sorted(rank_dict.items(), key = lambda x:x[1])) #Sort dictionary by value
+sorted_dict = {}
+for hand in set(rank_dict.values()): #For each type of hand after ranking
+    inter_rank_dict = {}
+    for item in rank_dict.items():
+        if item[1] == hand:
+            convert = str(item[0])
+            for x in range(len(cards)):
+                convert = convert.replace(cards[x], index[x]) #Converts hand into alphabet to enable ordering
+            inter_rank_dict.update({convert : info_dict.get(item[0])})
+    inter_rank_dict = dict(sorted(inter_rank_dict.items(), key = lambda x:x[0])) #Sort dictionary by key
+    sorted_dict.update(inter_rank_dict)
+sum = 0
+for x in range(len(sorted_dict.keys())):
+    sum += ((x+1) * list(sorted_dict.values())[x])
+print(sum)
