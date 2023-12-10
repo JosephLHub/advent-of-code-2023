7bca4a574a15b093a74c13ddb52e64b49341d21a
JosephLHub <125474970+JosephLHub@users.noreply.github.com>
2023-12-10 03:33:53 +0000

    completed day 9

---
+++ day09/day9-2.py
@@ -0,0 +1,12 @@
+import sys
+data = sys.stdin.read().split("\n")
+def main(diffs):
+    if not all(int(x) == 0 for x in diffs):
+        next_ = int(diffs[0]) - main([int(z) - int(y) for y, z in zip(diffs[:-1], diffs[1:])])
+    else:
+        next_ = 0
+    return next_
+sum = 0
+for history in data:
+    sum += main(history.split())
+print(sum)
