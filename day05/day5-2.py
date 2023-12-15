ee4d5199c59f55deb46096f61174266ab0def39e
JosephLHub <125474970+JosephLHub@users.noreply.github.com>
2023-12-15 02:47:29 +0000

    completed day 5 part 2

--- day05/day5-2.py
+++ day05/day5-2.py
@@ -2,22 +2,35 @@ import sys
 almanac = sys.stdin.read().split("\n")
 map_titles = ["seed-to-soil map:", "soil-to-fertilizer map:", "fertilizer-to-water map:", "water-to-light map:", "light-to-temperature map:", "temperature-to-humidity map:", "humidity-to-location map:"]
 seed_ranges = almanac[0][(almanac[0].index(":") + 2) :].split() #Obtain list of seed numbers at top of almanac
-seed_nums = []
+seed_nums = set()
 for x in range(len(seed_ranges)):
     if x % 2 == 0:
-        seed_nums += list(set(range(int(seed_ranges[x]), int(seed_ranges[x]) + int(seed_ranges[x+1]))))
+        seed_nums.update(range(int(seed_ranges[x]), int(seed_ranges[x]) + int(seed_ranges[x+1])))
         print("Done")
-print(len(seed_nums))
 for title in map_titles:
     current_map = almanac[almanac.index(title) + 1 : almanac.index("", almanac.index(title))] #Extract each map from almanac based on line preceeding it
-    for x in range(len(seed_nums)):
-        for section in current_map:
-            ranges = section.split()
-            if seed_nums[x] >= int(ranges[1]) and seed_nums[x] < (int(ranges[1]) + int(ranges[2])): #Check if number is within each range
-                seed_nums[x] = (int(ranges[0]) + (seed_nums[x] - int(ranges[1]))) #Start at destination range & add difference between previous number & start of source range
-                break #Ensure number cannot be modified further in this iteration
-    seed_nums = list(set(seed_nums))
+    new_nums = set()
+    for section in current_map:
+        ranges = [int(i) for i in section.split()]
+        min_ = ranges[1]
+        max_ = ranges[1] + ranges[2]
+        dest = ranges[0]
+        if len(seed_nums) > 0 and min(seed_nums) < max_:
+            if min(seed_nums) >= min_ and max(seed_nums) < max_: #If entire set of nums is in section
+                remove_nums = set([num for num in seed_nums])
+                new_nums.update([(dest + num - min_) for num in seed_nums])
+            elif min(seed_nums) >= min_: #If all nums are at least over the lower bound
+                remove_nums = set([num for num in seed_nums if num < max_])
+                new_nums.update([(dest + num - min_) for num in seed_nums if num < max_])
+            elif max(seed_nums) < max_: #If all nums are at most under the lower bound
+                remove_nums = set([num for num in seed_nums if min_ <= num])
+                new_nums.update([(dest + num - min_) for num in seed_nums if min_ <= num])
+            else: #If range of nums exceeds range of section
+                remove_nums = set([num for num in seed_nums if min_ <= num < max_])
+                new_nums.update([(dest + num - min_) for num in seed_nums if min_ <= num < max_])
+        print("section check")
+        seed_nums -= remove_nums
+        print(len(seed_nums))
+    seed_nums.update(new_nums)
     print(len(seed_nums))
 print(min(seed_nums))
-
-# answer = 79874951
